2b9fdb7af8d2365334b506cc6a0fa12b0ab2cd06
Stephanos Theodotou <stephanostheodotou@Stephanoss-MacBook-Pro.local>
2017-06-25 18:51:53 +0200

    add css class dynamic generation, add flexorder properties to grid system, add object properties to maze array, add initial attemp at custom depth-first-search algorithm

--- js/app.js
+++ js/app.js
@@ -2,33 +2,41 @@
 function Level(size){
     this.size = size;
     this.maze = this.generateMaze(this.levelSize);
-    
+
     //populate html with maze boxes
+    var n = 0;
     for(var a = 0; a < this.size; a++){
         for(var b = 0; b < this.size; b++){
-            $('.container').append($('<div>').addClass("box"));
+            n ++;
+            $('.container').append($('<div>').addClass("box").css("order", n));
         }
     }
-    
-    
 }
 
-
 //Level Constructor Prototypical Functions
-
-
 Level.prototype.generateMaze = function(arraySize){
+    var order = 0;
     var array =[];
     for(var x = 0; x < this.size; x++){
       array.push([]);
         for(var y = 0; y < this.size; y++){
-            array[x].push({name: 1111});
+            order ++;
+            array[x].push({objectOrder: order, top: 1, bottom: 1, left: 1, right : 1, visited: " "});
         }
     }
     return array;
 }
 
-
+Level.prototype.depthFirstSearch = function(){
+    for(var x = 0; x < this.size; this.maze[x++]){
+        for(var y = 0; y < this.size; this.maze[x][y++]){
+           do{
+               console.log("passing through " + this.maze[x][y].objectOrder);
+               this.maze[x][y].visited = true;
+           }while(this.maze[x][y+1] === false || this.maze[x+1][y] === false);
+        }
+    }
+}
 
  
 //JQUERY
@@ -36,6 +44,7 @@ $(document).ready(function() {
     
 level = new Level(10);
 console.log(level.maze);
+    console.log(level.depthFirstSearch());
             
             
             
