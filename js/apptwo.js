4fb8780282ffaeb7190d94bc9cadd0a6d1bfcc28
Stephanos Theodotou <stephanostheodotou@Stephanoss-MacBook-Pro.local>
2017-06-29 10:17:31 +0200

    add movement, add border functions, preserve hero object

--- js/apptwo.js
+++ js/apptwo.js
@@ -1,3 +1,11 @@
+//Constructor of the Hero Object
+function Hero(dimension){
+    
+ 
+}
+
+
+
 //Constructor of each new level
 function Level(size){
     this.size = size;
@@ -5,6 +13,9 @@ function Level(size){
     this.currentPos;
     this.matrix = [];
     this.arrayOfFns =[this.rotateRight,this.rotateLeft];
+    this.position = (size - 1) * size;
+    this.rightBorder = [];
+    
     
 //Populate html with maze cells
     var order = -1;
@@ -18,25 +29,35 @@ function Level(size){
                         }
                 
         }
-    
-}
 
-//Constructor of the Hero Object
-function Hero(size){
     
-    this.position = size * (size - 1);
-    this.targetPosition = $("[order=" +  this.position + "]").append("<div>");
-    this.targetPosition.find(">:first-child").addClass("red");
+this.reDraw();
+this.assignControlsToKeys();    
 
     
 }
-
+Level.prototype.applyBorderRight = function(size){
+    this.rightBorder.push(size - 1);
+    var counter = 0;
+    for(var x = 2; x < size+1; x++){
+        counter ++;
+        this.rightBorder.push(((size -1) * x) + counter);
+    }
+    console.log(this.rightBorder);
+}
 
 //Prototypical Functions of the level
 Level.prototype.timer = function(){
 
 }
 
+Level.prototype.reDraw = function(){
+    var make = $("[order=" +  this.position + "]");
+    console.log("what i give to redraw " + make);
+    $('.red').remove();
+    $(make).append($("<div>").addClass("red"));
+    //split append give it to constr
+}
 
 Level.prototype.rotateRight = function(){
     $('#board').animate({  borderSpacing: 90 }, 
@@ -64,27 +85,99 @@ Level.prototype.orchestration = function(randomNumber) {
     (fnToRun)();
 }
 
-
-//Prototypical functions of the Hero
-/*hero.prototype.addToBoard =  function(){
-    var make = $("[order=" +  this.position + "]");
-    $(make).addClass("red");
+Level.prototype.assignControlsToKeys = function() {
+  $(document).on('keydown', function(e) {
+    switch (e.keyCode) {
+      case 38: // arrow up
+        this.goUp();
+        break;
+      case 40: // arrow down
+        this.goDown();
+        break;
+      case 37: // arrow left
+        this.goLeft();
+        break;
+      case 39: // arrow right
+        this.goRight();
+        break;
+            /*
+      case 80: // p pause
+        if (this.intervalId) {
+          this.stop();
+        } else {
+          this.start();
+        }
+        break;*/
+    }
+  }.bind(this));
 }
-*/
 
+
+
+
+
+Level.prototype.goLeft = function() {
+    console.log("going left", level.position);
+    level.position -= 1;
+    this.reDraw();
+
+
+}
+
+Level.prototype.goRight = function() {
+    console.log("going right", level.position);
+    level.position += 1;
+    this.reDraw();
+
+}
+
+Level.prototype.goUp = function() {
+    console.log("going up", level.position);
+    level.position -= 8;
+    this.reDraw();
+
+
+}
+
+Level.prototype.goDown = function() {
+    console.log("going down", level.position);
+    level.position += 8; 
+    this.reDraw();
+
+}
 
 //========RUN THINGS HERE==========//
 $(document).ready(function() {
-    
     level = new Level(8);
-    hero = new Hero(8);
-    //level.timer();
-    level.orchestration(level.randomiseMovements(level.arrayOfFns));  
-    console.log(hero.position);
-    console.log(hero);
+        hero = new Hero(8);
+    
+    level.applyBorderRight(8);
 
+     
+    
+    //level.timer();
+    //level.orchestration(level.randomiseMovements(level.arrayOfFns));
+
+   //checks
+    
+    /*level.reDraw();
+    
+        level.goUp();
+        console.log(level.position);
+
+        level.reDraw();
+    
+            level.goUp();
+            console.log(level.position);
+
+            level.reDraw();
+
+                level.goUp();
+                console.log(level.position);
+
+                level.reDraw();
 
     //level.rotateRight();
     //level.rotateLeft();    
-    
-    });
+*/    
+});
